acbe627b9a8147f69ba387fd50457e2a0370e124
AR123456 <coloradoz99@gmail.com>
2018-07-26 18:07:16 -0600

    updates and clean ups to code

--- bamazon.sql
+++ bamazon.sql
@@ -4,25 +4,27 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
-CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  product VARCHAR(100) NULL,
-  department VARCHAR(100) NULL,
-  price DECIMAL(10,2) NULL,
+CREATE TABLE products
+(
+  id INT NOT NULL
+  AUTO_INCREMENT,
+  product VARCHAR
+  (100) NULL,
+  department VARCHAR
+  (100) NULL,
+  price DECIMAL
+  (10,2) NULL,
   stock INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY
+  (id)
 );
 
   USE bamazon;
-  INSERT INTO products (product, department, price, stock)
-  VALUES ("Swifer", "Home Goods", 12.99, 10);
+  INSERT INTO products
+    (product, department, price, stock)
+  VALUES
+    ("Swifer", "Home Goods", 12.99, 10);
 
-  SELECT * FROM products;
-  -- using csv file for other products 
-
-
-  -- USE bamazon;
-  -- INSERT INTO products
-  --   (product_name, department_name, price, quantity)
-  -- VALUES
-  --   ("Echo", "Electronics", 179.00, 10);
+  SELECT *
+  FROM products;
+ 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,9 @@
 //required NPMs
 var inquirer = require("inquirer");
 var fs = require("fs");
-//https://www.npmjs.com/package/mysql
+
 var mysql = require("mysql");
-//connect to the db
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,18 +11,17 @@ var connection = mysql.createConnection({
   password: "midnight1",
   database: "bamazon"
 });
-//check if connection worked then begin the program
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 });
-// display the products table 
+
 connection.query("SELECT * FROM products", function(err, res) {
   if (err) throw err;
-  // Log all results of the SELECT statement
-  // console.log(res);
   console.log("----------Products for sale----------------------");
-  for (var i = 0; i < res.length; i++) { console.log(
+  for (var i = 0; i < res.length; i++) {
+    console.log(
       res[i].id +
         " | " +
         res[i].product +
@@ -35,7 +34,7 @@ connection.query("SELECT * FROM products", function(err, res) {
     );
   }
   console.log("------------------------------------------------");
-  ///customer prompts //
+
   inquirer
     .prompt([
       {
@@ -51,8 +50,6 @@ connection.query("SELECT * FROM products", function(err, res) {
       }
     ])
     .then(function(answer) {
-      //check store stock, in not enough stop order and display insufficient quantity
-      // var itemID = answer.whatpurchase;
       connection.query(
         "SELECT * FROM products WHERE ?",
         { id: answer.whatpurchase },
@@ -82,34 +79,39 @@ connection.query("SELECT * FROM products", function(err, res) {
                   "Number of " + item.product + "(s) left: " + stockRemaining
                 );
               }
-            ); //end update products
+            );
           } else {
-            // end of if its in stock
             console.log("Insufficient quantity.");
             return "Insufficient quantity.";
-          } // end of else not enough
+          }
 
-          // department logic here need total sales - add a column for this in the database then pull it for app display 
-          				// put total cost into totalSales column for the related department
-
-				connection.query('SELECT * FROM products WHERE ?', {department: item.department}, function(err, res ) {
-          if (err) throw err;
-  
-          var total = sale + res[0].totalSales;
-  
-          connection.query('UPDATE products SET ? WHERE ?', [{
-                  totalSales: total
-              }, {
-                  department: item.department
-              }], function(err, res) {
+          connection.query(
+            "SELECT * FROM products WHERE ?",
+            { department: item.department },
+            function(err, res) {
               if (err) throw err;
-              
-              console.log(item.department + "'s total sales are: $" + total);
-           }); // end of query update departments in product table 
-  
-          }); // end of connrvyion query for select department 
+              var total = sale + res[0].totalSales;
+              connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    totalSales: total
+                  },
+                  {
+                    department: item.department
+                  }
+                ],
+                function(err, res) {
+                  if (err) throw err;
 
+                  console.log(
+                    item.department + "'s total sales are: $" + total
+                  );
+                }
+              ); 
+            }
+          ); /
         }
       );
-    }); //user prompt
-}); //this is the connection function
+    }); 
+});
